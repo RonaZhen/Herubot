2a4b4550c54d03887e62522764f80786bd737fe5
RonaZhen <zhenheru1@gmail.com>
2024-07-14 18:43:59 +0800

    Update ai3.js

--- scr/cmd/ai3.js
+++ scr/cmd/ai3.js
@@ -1,16 +1,18 @@
 const axios = require("axios");
 
-module.exports = {
-  config: {
+module["exports"] = class {
+  static config = {
     name: "ai3",
     description: "Query the GPT-4 API",
     usage: "[question]",
     cooldown: 5,
     accessableby: 0,
     category: "AI",
-    prefix: false
-  },
-  start: async function ({ api, text, react, event, reply }) {
+    prefix: false,
+    author: "heru"
+  };
+
+  static async start({ api, text, react, event, reply }) {
     const query = text.join(" ");
 
     if (!query) {
@@ -18,12 +20,14 @@ module.exports = {
     }
 
     try {
+      react("⏳");
       const response = await axios.get(`https://my-api-v1.onrender.com/api/v1/gpt4?ask=${encodeURIComponent(query)}`);
-      
+
       // Check if the response structure is as expected
-      if (response.data && response.data.answer) {
-        const answer = response.data.answer;
-        return reply("🤖 | 𝙶𝚙𝚝4 (𝚗𝚘𝚗-𝚌𝚘𝚗𝚟𝚎𝚛𝚜𝚊𝚝𝚒𝚘𝚗𝚊𝚕)\n━━━━━━━━━━━━━━━━━━\n" + answer);
+      if (response.data && response.data.response) {
+        const answer = response.data.response;
+        react('✅');
+        return reply("🤖 | 𝙶𝚙𝚝4 (𝚗𝚘𝚗-𝚌𝚘𝚗𝚟𝚎𝚛𝚜𝚊𝚝𝚒𝚘𝚗𝚊𝚕)\n━━━━━━━━━━━━━━━━━━\n" + response);
       } else {
         throw new Error("Unexpected API response structure.");
       }
@@ -31,8 +35,9 @@ module.exports = {
       console.error(`Error querying the API: ${error.message}`);
       return reply("Failed to query the API. Please try again later.");
     }
-  },
-  auto: async function ({ api, event, text, reply }) {
+  }
+
+  static async auto({ api, event, text, reply }) {
     // No auto functionality for this command
   }
 };
