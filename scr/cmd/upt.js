ddfb0b52e25d7ef435d1370e64affaa1cf494c10
RonaZhen <zhenheru1@gmail.com>
2024-07-16 21:22:29 +0800

    Update upt.js

--- scr/cmd/upt.js
+++ scr/cmd/upt.js
@@ -1,15 +1,31 @@
-module.exports.start = async function ({ api, event }) {
+module.exports.start = async function ({ text, reply }) {
+  const axios = require("axios");
+  const randomUID = Math.floor(Math.random() * 20) + 1; // Generates a random number between 1 and 20
   const time = process.uptime();
   const hours = Math.floor(time / (60 * 60));
   const minutes = Math.floor((time % (60 * 60)) / 60);
   const seconds = Math.floor(time % 60);
-  return api.shareContact("Bot is running " + hours + " hour(s), " + minutes + " minute(s), and " + seconds + " second(s)", api.getCurrentUserID(), event.threadID);
+
+  const res = (
+    await axios.get(
+      `https://joshweb.click/canvas/uptime?id=${randomUID}&instag=heru&ghub=herudev&fb=Jay%20Mar&hours=${hours}&minutes=${minutes}&seconds=${seconds}&botname=Heru%20Bot`,
+      {
+        responseType: "stream"
+      }
+    )
+  ).data;
+
+  return reply({
+    body: `⌛ Hours: ${hours}\nMinutes: ${minutes}\nSeconds: ${seconds}\nName: Heru Bot\nOwner: Jay Mar\nUID: ${randomUID}`,
+    attachment: res
+  });
 };
+
 module.exports.config = {
   name: "upt",
   prefix: false,
   accessibleby: 0,
   description: "Upt",
   credits: "Deku",
-  category: "system",
+  category: "system"
 };
